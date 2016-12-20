153005e3ebe13cdc1348faee8501534442ed0ec8
Marius Volkhart <Marius@volkhart.com>
2016-12-19 19:41:03 -0600

    Support tablets

--- app/src/main/java/com/volkhart/lyrics/MainActivity.java
+++ app/src/main/java/com/volkhart/lyrics/MainActivity.java
@@ -7,20 +7,30 @@ import timber.log.Timber;
 
 public final class MainActivity extends AppCompatActivity {
 
+    private boolean showMasterDetail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showMasterDetail = getResources().getBoolean(R.bool.showMasterDetail);
+        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             if (BuildConfig.DEBUG) {
                 Timber.plant(new Timber.DebugTree());
             }
             getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, SongIndexFragment.newInstance(), SongIndexFragment.TAG)
+                    .replace(R.id.master, SongIndexFragment.newInstance(), SongIndexFragment.TAG)
                     .commit();
         }
     }
 
     public void showSong(Song song) {
-        startActivity(SongDetailsActivity.newIntent(this, song));
+        if (showMasterDetail) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail, SongFragment.newInstance(song), SongFragment.TAG)
+                    .commit();
+        } else {
+            startActivity(SongDetailsActivity.newIntent(this, song));
+        }
     }
 }
