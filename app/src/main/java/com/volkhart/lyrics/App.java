016d7f7f8c3b14f67d862c221c1461017aa7d315
Marius Volkhart <Marius@volkhart.com>
2017-11-04 18:40:36 +0700

    Enable StrictMode

--- app/src/main/java/com/volkhart/lyrics/App.java
+++ app/src/main/java/com/volkhart/lyrics/App.java
@@ -1,6 +1,7 @@
 package com.volkhart.lyrics;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.volkhart.feedback.FeedbackTree;
 
@@ -14,6 +15,21 @@ public final class App extends Application {
         Timber.plant(FeedbackTree.INSTANCE);
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        }
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .penaltyDeath()
+                    .build());
         }
     }
 }
